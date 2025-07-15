7cae2fdd64fad25530fdc3974e31b7c11a72b94e
L1nSn0w <l1nsn0w@qq.com>
2025-07-15 12:44:03 +0800

    fix: 移除欢迎信息的延迟显示，确保立即输出

--- web/src/plugins/welcome-plugin.ts
+++ web/src/plugins/welcome-plugin.ts
@@ -11,10 +11,7 @@ export function welcomePlugin(): Plugin {
       server.middlewares.use('/', (req, res, next) => {
         if (!hasShown) {
           // 延迟显示，确保 Vite 的启动信息已经输出完成
-          setTimeout(() => {
-            console.log('\n') // 添加一些间距
-            printWelcome()
-          }, 0) // 1.5秒延迟，确保 Vite 完全启动
+          printWelcome()
           hasShown = true
         }
         next()
