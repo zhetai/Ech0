d95d7b89293b735f5871d6c79b19828f367497ff
L1nSn0w <l1nsn0w@qq.com>
2025-08-03 13:44:43 +0800

    feat: add cache comment

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -7,6 +7,7 @@ import (
 	userModel "github.com/lin-snow/ech0/internal/model/user"
 )
 
+// ICache 定义了缓存接口，提供基本的缓存操作方法
 type ICache[K ristretto.Key, V any] interface {
 	Set(key K, value V, cost int64) bool
 	Get(key K) (V, error)
@@ -14,15 +15,18 @@ type ICache[K ristretto.Key, V any] interface {
 	GetOrSet(key K, cost int64, fn func() (V, error)) (V, error)
 }
 
+// NewCache 创建一个新的缓存实例，使用 Ristretto 作为缓存实现
 func NewCache[K ristretto.Key, V any]() (ICache[K, V], error) {
 	return NewRistrettoCache[K, V](1000000, 1000000, 100)
 }
 
+// CacheFactory 是一个工厂类，用于创建和管理不同类型的缓存实例
 type CacheFactory struct {
 	userCache ICache[string, *userModel.User]
 	echoCache ICache[string, commonModel.PageQueryResult[[]echoModel.Echo]]
 }
 
+// NewCacheFactory 创建一个新的 CacheFactory 实例，并初始化所需的缓存
 func NewCacheFactory() *CacheFactory {
 	userCache, err := NewCache[string, *userModel.User]()
 	if err != nil {
@@ -40,10 +44,12 @@ func NewCacheFactory() *CacheFactory {
 	}
 }
 
+// UserCache 返回用户缓存实例
 func (f *CacheFactory) UserCache() ICache[string, *userModel.User] {
 	return f.userCache
 }
 
+// EchoCache 返回 Echo 缓存实例
 func (f *CacheFactory) EchoCache() ICache[string, commonModel.PageQueryResult[[]echoModel.Echo]] {
 	return f.echoCache
 }
--- internal/cache/restretto.go
+++ internal/cache/restretto.go
@@ -6,10 +6,12 @@ import (
 	"github.com/dgraph-io/ristretto/v2"
 )
 
+// RistrettoCache 是基于 Ristretto 实现的缓存结构体
 type RistrettoCache[K ristretto.Key, V any] struct {
 	cache *ristretto.Cache[K, V]
 }
 
+// NewRistrettoCache 创建一个新的 RistrettoCache 实例
 func NewRistrettoCache[K ristretto.Key, V any](maxCost int64, numCounters int64, bufferItems int64) (*RistrettoCache[K, V], error) {
 	cache, err := ristretto.NewCache(&ristretto.Config[K, V]{
 		NumCounters: numCounters,
@@ -22,10 +24,12 @@ func NewRistrettoCache[K ristretto.Key, V any](maxCost int64, numCounters int64,
 	return &RistrettoCache[K, V]{cache: cache}, nil
 }
 
+// Set 将键值对存入缓存
 func (r *RistrettoCache[K, V]) Set(key K, value V, cost int64) bool {
 	return r.cache.Set(key, value, cost)
 }
 
+// Get 从缓存中获取值，如果不存在则返回错误
 func (r *RistrettoCache[K, V]) Get(key K) (V, error) {
 	value, found := r.cache.Get(key)
 	if !found {
@@ -36,10 +40,12 @@ func (r *RistrettoCache[K, V]) Get(key K) (V, error) {
 	return value, nil
 }
 
+// Delete 从缓存中删除指定的键
 func (r *RistrettoCache[K, V]) Delete(key K) {
 	r.cache.Del(key)
 }
 
+// GetOrSet 尝试从缓存中获取值，如果不存在则调用提供的函数获取值并存入缓存
 func (r *RistrettoCache[K, V]) GetOrSet(key K, cost int64, fn func() (V, error)) (V, error) {
 	value, found := r.cache.Get(key)
 	if found {
