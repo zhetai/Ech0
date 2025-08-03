5b3dab195288c309b8d218bf63585cbabc96ab5a
L1nSn0w <l1nsn0w@qq.com>
2025-08-03 12:04:41 +0800

    feat: 更新数据备份和导出备份函数的注释，增强API文档的清晰度

--- internal/handler/backup/backup.go
+++ internal/handler/backup/backup.go
@@ -19,7 +19,17 @@ func NewBackupHandler(backupService service.BackupServiceInterface) *BackupHandl
 	}
 }
 
-// Backup 执行备份
+
+// Backup 执行数据备份
+//
+// @Summary 执行数据备份
+// @Description 用户触发数据备份操作，成功后返回备份成功信息
+// @Tags 系统备份
+// @Accept json
+// @Produce json
+// @Success 200 {object} res.Response "备份成功"
+// @Failure 200 {object} res.Response "备份失败"
+// @Router /backup [get]
 func (backupHandler *BackupHandler) Backup() gin.HandlerFunc {
 	return res.Execute(func(ctx *gin.Context) res.Response {
 		userId := ctx.MustGet("userid").(uint)
@@ -36,7 +46,16 @@ func (backupHandler *BackupHandler) Backup() gin.HandlerFunc {
 	})
 }
 
-// ExportBackup 导出备份
+// ExportBackup 导出数据备份
+//
+// @Summary 导出数据备份
+// @Description 用户导出备份文件，成功后触发文件下载
+// @Tags 系统备份
+// @Accept json
+// @Produce application/octet-stream
+// @Success 200 {object} res.Response "导出备份成功，返回文件下载"
+// @Failure 200 {object} res.Response "导出备份失败"
+// @Router /backup/export [get]
 func (backupHandler *BackupHandler) ExportBackup() gin.HandlerFunc {
 	return res.Execute(func(ctx *gin.Context) res.Response {
 		userId := ctx.MustGet("userid").(uint)
