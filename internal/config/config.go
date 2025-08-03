fb1b12d5d0a7eb01c92e6a7d4b5332fcd8e9e085
L1nSn0w <l1nsn0w@qq.com>
2025-08-03 13:34:45 +0800

    feat: 更新配置结构体注释，增强代码可读性

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,41 +21,40 @@ var JWT_SECRET []byte
 // AppConfig 应用程序配置结构体
 type AppConfig struct {
 	Server struct {
-		Port string `yaml:"port"`
-		Host string `yaml:"host"`
-		Mode string `yaml:"mode"`
+		Port string `yaml:"port"`  // 服务器端口
+		Host string `yaml:"host"` // 服务器主机地址
+		Mode string `yaml:"mode"` // 运行模式，可能的值为 "debug" 或 "release"
 	} `yaml:"server"`
 	Database struct {
-		Type string `yaml:"type"`
-		Path string `yaml:"path"`
-		// Pragma string `yaml:"pragma"`
+		Type string `yaml:"type"` // 数据库类型
+		Path string `yaml:"path"` // 数据库文件路径
 	} `yaml:"database"`
 	Auth struct {
 		Jwt struct {
-			Expires  int    `yaml:"expires"`
-			Issuer   string `yaml:"issuer"`
-			Audience string `yaml:"audience"`
+			Expires  int    `yaml:"expires"` // JWT的过期时间，单位为秒
+			Issuer   string `yaml:"issuer"` // JWT的发行者
+			Audience string `yaml:"audience"` // JWT的受众
 		} `yaml:"jwt"`
 	} `yaml:"auth"`
 	Upload struct {
-		ImageMaxSize int      `yaml:"imagemaxsize"`
-		AudioMaxSize int      `yaml:"audiomaxsize"`
-		AllowedTypes []string `yaml:"allowedtypes"`
-		ImagePath    string   `yaml:"imagepath"`
-		AudioPath    string   `yaml:"audiopath"`
+		ImageMaxSize int      `yaml:"imagemaxsize"` // 图片文件的最大上传大小，单位为字节
+		AudioMaxSize int      `yaml:"audiomaxsize"` // 音频文件的最大上传大小，单位为字节
+		AllowedTypes []string `yaml:"allowedtypes"` // 允许上传的文件类型
+		ImagePath    string   `yaml:"imagepath"` // 图片文件存储路径
+		AudioPath    string   `yaml:"audiopath"` // 音频文件存储路径
 	} `yaml:"upload"`
 	Setting struct {
-		SiteTitle     string `yaml:"sitetitle"`
-		Servername    string `yaml:"servername"`
-		Serverurl     string `yaml:"serverurl"`
-		AllowRegister bool   `yaml:"allowregister"`
-		Icpnumber     string `yaml:"icpnumber"`
-		MetingAPI     string `yaml:"metingapi"`
-		CustomCSS     string `yaml:"customcss"`
-		CustomJS      string `yaml:"customjs"`
+		SiteTitle     string `yaml:"sitetitle"` // 网站标题
+		Servername    string `yaml:"servername"` // 服务器名称
+		Serverurl     string `yaml:"serverurl"` // 服务器 URL
+		AllowRegister bool   `yaml:"allowregister"` // 是否允许注册
+		Icpnumber     string `yaml:"icpnumber"` // ICP 备案号
+		MetingAPI     string `yaml:"metingapi"` // Meting API 地址
+		CustomCSS     string `yaml:"customcss"` // 自定义 CSS 样式
+		CustomJS      string `yaml:"customjs"` // 自定义 JS 脚本
 	} `yaml:"setting"`
 	Comment struct {
-		EnableComment bool   `yaml:"enablecomment"`
+		EnableComment bool   `yaml:"enablecomment"` // 是否启用评论
 		Provider      string `yaml:"provider"`   // 评论提供者
 		CommentAPI    string `yaml:"commentapi"` // 评论 API 地址
 	} `yaml:"comment"`
