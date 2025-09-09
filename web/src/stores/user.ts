50763fa7385a1cfe329628bd49c73b0babc3cb85
L1nSn0w <l1nsn0w@qq.com>
2025-09-09 17:48:53 +0800

    feat: 登录成功后刷新Echo组件

--- web/src/stores/user.ts
+++ web/src/stores/user.ts
@@ -5,6 +5,7 @@ import { saveAuthToken } from '@/service/request/shared'
 import { localStg } from '@/utils/storage'
 import { theToast } from '@/utils/toast'
 import { useRouter } from 'vue-router'
+import { useEchoStore } from './echo'
 
 export const useUserStore = defineStore('userStore', () => {
   /**
@@ -32,6 +33,10 @@ export const useUserStore = defineStore('userStore', () => {
         // 登录成功
         theToast.success('登录成功')
         router.push({ name: 'home' })
+
+        // 登录后刷新Echo
+        const echoStore = useEchoStore()
+        echoStore.refreshEchos()
       }
     })
   }
