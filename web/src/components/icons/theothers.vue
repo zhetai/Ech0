0d5bd394fcd9ae33a595afd407ebcbd26182a1cb
L1nSn0w <l1nsn0w@qq.com>
2025-09-28 20:42:56 +0800

    feat: 更新图标组件，替换为新的 SVG 图形以提升视觉效果

--- web/src/components/icons/theothers.vue
+++ web/src/components/icons/theothers.vue
@@ -1,17 +1,8 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-    <!-- Icon from Lucide by Lucide Contributors - https://github.com/lucide-icons/lucide/blob/main/LICENSE -->
-    <g fill="none" stroke="#888888" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-      <ellipse cx="12" cy="5" rx="9" ry="3" />
-      <path d="M3 12a9 3 0 0 0 5 2.69M21 9.3V5" />
-      <path d="M3 5v14a9 3 0 0 0 6.47 2.88M12 12v4h4" />
-      <path d="M13 20a5 5 0 0 0 9-3a4.5 4.5 0 0 0-4.5-4.5c-1.33 0-2.54.54-3.41 1.41L12 16" />
-    </g>
-  </svg>
+	<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256" viewBox="0 0 256 256">
+		<g fill="#888888">
+			<path d="M208 40h-32v40a8 8 0 0 1-8 8H88a8 8 0 0 1-8-8V40.73a8 8 0 0 0-2.34 1.61L42.34 77.66A8 8 0 0 0 40 83.31V208a8 8 0 0 0 8 8h160a8 8 0 0 0 8-8V48a8 8 0 0 0-8-8m-80 144a32 32 0 1 1 32-32a32 32 0 0 1-32 32" opacity="0.2" />
+			<path d="M208 32H83.31A15.86 15.86 0 0 0 72 36.69L36.69 72A15.86 15.86 0 0 0 32 83.31V208a16 16 0 0 0 16 16h160a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16M88 48h80v32H88Zm120 160H48V83.31l24-24V80a16 16 0 0 0 16 16h80a16 16 0 0 0 16-16V48h24Zm-80-96a40 40 0 1 0 40 40a40 40 0 0 0-40-40m0 64a24 24 0 1 1 24-24a24 24 0 0 1-24 24" />
+		</g>
+	</svg>
 </template>
-
-<script lang="ts">
-export default {
-  name: 'LucideDatabaseBackup',
-}
-</script>
