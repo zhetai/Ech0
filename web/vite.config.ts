fff68725ef05adfb75af2f8ba4c2578099930901
L1nSn0w <l1nsn0w@qq.com>
2025-09-30 14:59:49 +0800

    feat: add gzip compression plugin to Vite configuration

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import UnoCSS from 'unocss/vite'
+import viteCompression from 'vite-plugin-compression';
 
 import { welcomePlugin } from './src/plugins/welcome-plugin'
 
@@ -13,6 +14,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     UnoCSS(),
+    viteCompression({
+      deleteOriginFile: true,
+    }),
 
     welcomePlugin() // 欢迎横幅插件
   ],
