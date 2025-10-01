5ca85ff4fd341cfa5a622b1e15d14919b4789a8a
L1nSn0w <l1nsn0w@qq.com>
2025-10-01 12:12:02 +0800

    feat: 将所有公共路由更改为资源路由以统一路由管理

--- internal/router/fediverse.go
+++ internal/router/fediverse.go
@@ -4,23 +4,23 @@ import "github.com/lin-snow/ech0/internal/di"
 
 func setupFediverseRoutes(appRouterGroup *AppRouterGroup, h *di.Handlers) {
 	// // ActivityPub discovery
-	appRouterGroup.PublicRouterGroup.GET("/.well-known/webfinger", h.FediverseHandler.Webfinger)
+	appRouterGroup.ResourceGroup.GET("/.well-known/webfinger", h.FediverseHandler.Webfinger)
 
 	// Actor (用户资料)
 	appRouterGroup.ResourceGroup.GET("/users/:username", h.FediverseHandler.GetActor)
 
 	// Inbox (接收 ActivityPub 消息)
-	appRouterGroup.PublicRouterGroup.POST("/users/:username/inbox", h.FediverseHandler.PostInbox)
+	appRouterGroup.ResourceGroup.POST("/users/:username/inbox", h.FediverseHandler.PostInbox)
 
 	// Outbox (发布消息)
-	appRouterGroup.PublicRouterGroup.GET("/users/:username/outbox", h.FediverseHandler.GetOutbox)
+	appRouterGroup.ResourceGroup.GET("/users/:username/outbox", h.FediverseHandler.GetOutbox)
 
 	// Followers list
-	appRouterGroup.PublicRouterGroup.GET("/users/:username/followers", h.FediverseHandler.GetFollowers)
+	appRouterGroup.ResourceGroup.GET("/users/:username/followers", h.FediverseHandler.GetFollowers)
 
 	// Following list
-	appRouterGroup.PublicRouterGroup.GET("/users/:username/following", h.FediverseHandler.GetFollowing)
+	appRouterGroup.ResourceGroup.GET("/users/:username/following", h.FediverseHandler.GetFollowing)
 
 	// Objects (内容对象访问)
-	appRouterGroup.PublicRouterGroup.GET("/objects/:id", h.FediverseHandler.GetObject)
+	appRouterGroup.ResourceGroup.GET("/objects/:id", h.FediverseHandler.GetObject)
 }
