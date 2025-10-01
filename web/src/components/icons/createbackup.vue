8c77f9da41107aa4637049d0f58d374ddc3ac0e2
L1nSn0w <l1nsn0w@qq.com>
2025-10-01 11:50:36 +0800

    feat: 更新图标组件，添加缺失的描边属性以增强可视化效果

--- web/src/components/icons/createbackup.vue
+++ web/src/components/icons/createbackup.vue
@@ -8,6 +8,7 @@
       y="36.522"
       fill="none"
       stroke="#888888"
+      stroke-width="3"
       stroke-linecap="round"
       stroke-linejoin="round"
       rx="2.991"
@@ -15,6 +16,7 @@
     <path
       fill="none"
       stroke="#888888"
+      stroke-width="3"
       stroke-linecap="round"
       stroke-linejoin="round"
       d="M42.085 30.81a2.774 2.774 0 0 0-2.775-2.774H8.69a2.774 2.774 0 0 0-2.775 2.774h0M38.326 7.693a2.2 2.2 0 0 0-2.198-2.198H11.872a2.2 2.2 0 0 0-2.198 2.198h0m0 0L4.5 39.513m33.826-31.82L43.5 39.514m-24.942-.001h10.884"
@@ -24,6 +26,7 @@
     <path
       fill="none"
       stroke="#888888"
+      stroke-width="3"
       stroke-linecap="round"
       stroke-linejoin="round"
       d="M24 24.649c5.41 0 9.384-3.894 8.923-8.269c-.419-3.965-4.398-6.916-8.923-6.916s-8.504 2.951-8.923 6.916c-.267 2.531 2.219 4.072 2.219 4.072l1.954-4.072h-4.173"
