4bbc2e62db55a5ecdcd52a8d6cb65e54d9e36c0a
L1nSn0w <l1nsn0w@qq.com>
2025-10-27 20:28:02 +0800

    feat: 修复 NoCache 中间件中的 Next 调用顺序，以确保正确处理请求

--- internal/middleware/nocache.go
+++ internal/middleware/nocache.go
@@ -4,6 +4,8 @@ import "github.com/gin-gonic/gin"
 
 func NoCache() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		c.Next()
+
 		// 所有缓存节点（浏览器、CDN、代理）完全不要存储响应内容
 		c.Header("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0")
 		// 针对 HTTP/1.0 兼容
@@ -12,6 +14,5 @@ func NoCache() gin.HandlerFunc {
 		c.Header("Expires", "0")
 		// 针对 Surrogate 缓存（如 CDN）
 		c.Header("Surrogate-Control", "no-store")
-		c.Next()
 	}
 }
